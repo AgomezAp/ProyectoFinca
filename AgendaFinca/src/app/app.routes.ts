24f2478a8e1f4dda35423c7991b6e46e393f8526
Inge-AP <aagudelo.investigacion@andrespublicidadtg.com>
2025-07-09 08:35:22 -0500

    Add Data Protection Policy and Terms & Conditions components
    - Implemented the Data Protection Policy component with HTML and TypeScript files.
    - Created the Terms & Conditions component including detailed terms, conditions, and styling.
    - Added CSS for both components to enhance visual presentation and user experience.

--- AgendaFinca/src/app/app.routes.ts
+++ AgendaFinca/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { ReservaComponent } from './components/reserva/reserva.component'
 import { VerificarComponent } from './components/verificar/verificar.component';
 import { PanelComponent } from './components/panel/panel.component';
 import { InfoContactoComponent } from './components/info-contacto/info-contacto.component';
+import { DatosPersonalesComponent } from './components/politicas/datos-personales/datos-personales.component';
+import { TerminosycondicionesComponent } from './components/politicas/terminosycondiciones/terminosycondiciones.component';
 import { correoGuard } from './core/correo.guard';
 
 export const routes: Routes = [
@@ -12,7 +14,9 @@ export const routes: Routes = [
     {path: 'reserva', component: ReservaComponent},
     {path: 'miReserva', component: VerificarComponent},
     {path: 'panel', component: PanelComponent , canActivate: [correoGuard]},
-    {path: 'about', component: InfoContactoComponent}
+    {path: 'about', component: InfoContactoComponent},
+    {path: 'manejodatos', component: DatosPersonalesComponent },
+    {path: 'terminos', component: TerminosycondicionesComponent}
 
 
 ];
